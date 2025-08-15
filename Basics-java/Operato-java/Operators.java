fe71074c6e8c73432a089110c052b4235772d8db
shubham <shubhamuprade0@gmail.com>
2025-08-15 11:17:06 +0530

    opeartors

---
+++ Basics-java/Operato-java/Operators.java
@@ -0,0 +1,49 @@
+// Arithmatics Operator
+
+// public class Operators {
+
+//     public static void main(String[] args) {
+//         int a = 10 ;
+//         int b = 20 ;
+//         System.out.println(a+b);
+//         System.out.println(a-b);
+//         System.out.println(a*b);
+//         System.out.println(a/b);
+//         System.out.println(a%b);
+
+//     }
+// }
+
+
+// Relation Operator
+
+// public class Operators {
+
+//     public static void main(String[] args) {
+//         int a = 10 ;
+//         int b = 20 ;
+//         System.out.println(a==b);
+//         System.out.println(a!=b);
+//         System.out.println(a>b);
+//         System.out.println(a<b);
+//         System.out.println(a>=b);
+//         System.out.println(a<=b);
+
+//     }
+// }
+
+// Logical Opearators
+
+// public class Operators {
+//     public static void main(String[] args) {
+//     int a = 10 ;
+//     int b = 20;
+//     System.out.println(a>10 && b>20);
+//     System.out.println(a<10 || b<20);
+//     System.out.println(a<10 && b>20);
+
+
+//     }
+// }
+
+
